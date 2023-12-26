d557c8a11f944901f74ba784332de4c13389c5c1
joão <joaopedrot1912@gmail.com>
2023-12-26 02:31:42 -0300

    env

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,22 +1,21 @@
 import { Sequelize } from 'sequelize-typescript';
+import { env } from '..';
 
+type Dialect = 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | undefined;
 
-//type Dialect = 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | undefined;
-
-//const dialect: Dialect = process.env.DB_DIALECT as Dialect || 'postgres';
+const dialect: Dialect = process.env.DB_DIALECT as Dialect || 'postgres';
 
 const database = new Sequelize({
-  dialect:'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'angelica125',
-  database: 'arquiteturaLimpa',
+  dialect,
+  host: env?.DB_HOST,
+  username: env?.DB_USERNAME,
+  password: env?.DB_PASSWORD,
+  database: env?.DB_NAME,
   models: [__dirname + '/src/database/models']
 });
 
 (async function(){
   await database.sync()
 })()
-
 
 export default database;
